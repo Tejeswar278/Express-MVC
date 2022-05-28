d65a85fd33edac7825dadb4c6cc8174dc4b6d471
Muddada Tejeswara Rao <lsandptlove143@gmail.com>
2022-05-28 07:59:35 +0530

    few changes

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = () => {
   return mongoose.connect(
-    "mongodb+srv://muralimv:murali12345@masai.g7gpy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    "mongodb+srv://epj:epj23@Azfqw@cluster0.uklfn.mongodb.net/?retryWrites=true&w=majority"
   );
 };
 module.exports=connect;
